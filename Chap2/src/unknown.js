12235877948b6eefdaf5ce956dc1a6aa7481d9c3
David Choi <dharric@live.com>
2020-02-11 07:37:14 -0500

    unknown

---
+++ Chap2/src/unknown.js
@@ -0,0 +1,7 @@
+var val = 10;
+val = "hello";
+val = new Object();
+val = new Array();
+val.push(22);
+var newval = val;
+console.log(newval);
---
+++ Chap2/src/unknown.ts
@@ -0,0 +1,7 @@
+let val: any = 10;
+val = "hello";
+val = new Object();
+val = new Array();
+val.push(22);
+let newval = val;
+console.log(newval);
