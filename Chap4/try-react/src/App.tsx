a9295b3873239bd747c4c2bc585e16d32ee44b41
David Choi <dharric@live.com>
2020-03-26 21:41:21 -0400

    chap 4 add Home comp

--- Chap4/try-react/src/App.tsx
+++ Chap4/try-react/src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
       </header>
     </div>
   );
---
+++ Chap4/try-react/src/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Home: FC = () => {
+  return <div>Hello World!</div>;
+};
+
+export default Home;
