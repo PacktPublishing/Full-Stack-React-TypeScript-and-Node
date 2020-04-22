e4df44d0bfd67170e2da0771fc7c47cb8b38511a
David Choi <dharric@live.com>
2020-04-22 10:51:30 -0400

    update reduce.js in chap3

--- Chap3/reduce.js
+++ Chap3/reduce.js
@@ -1,19 +1,12 @@
 const allTrucks = [
-    { id: 'x123', capacity: 2 },
-    { id: 'y2343', capacity: 5 },
-    { id: 'a9789', capacity: 6 },
-    { id: 'd2342', capacity: 10 },
+    2,5,7,10
 ]
 
-const allTonnage = allTrucks.reduce((totals, item) => {
-    if(!totals || !totals.capacity) {
-        totals = {
-            id: '0',
-            capacity: 0
-        }
-    }
-    totals.capacity = Number(totals.capacity) + Number(item.capacity);
+const initialCapacity = 0;
+const allTonnage = allTrucks.reduce((totalCapacity, currentCapacity) => {    
+    totalCapacity = totalCapacity + currentCapacity;
     
-    return totals;
-}, 0);
-console.log(allTonnage.capacity);
+    return totalCapacity;
+}, initialCapacity);
+
+console.log(allTonnage);
