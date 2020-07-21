34ed4be95c5ff775d60e36f116cebc932777265d
David Choi <dharric@live.com>
2020-07-21 15:09:53 -0400

    chap8 node-server nodemon

--- Chap8/node-server/server.mjs
+++ Chap8/node-server/server.mjs
@@ -1,11 +1,28 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("hello world");
+  if (req.url === "/") {
+    res.end("hello world");
+  } else if (req.url === "/a") {
+    res.end("welcome to route a");
+  } else if (req.url === "/b") {
+    res.end("welcome to route b");
+  } else if (req.url === "/c" && req.method === "POST") {
+    let body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const params = Buffer.concat(body);
+      console.log("body", params.toString());
+      res.end(`You submitted these parameters: ${params.toString()}`);
+    });
+  } else {
+    res.end("good bye");
+  }
 });
 
 const port = 8000;
 server.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`The server started on port ${port}`);
 });
