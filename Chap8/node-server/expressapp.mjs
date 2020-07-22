fa2880ee5f539e44edeb75310341f1016ebe16ca
David Choi <dharric@live.com>
2020-07-22 19:43:19 -0400

    finish chap8

--- Chap8/node-server/expressapp.mjs
+++ Chap8/node-server/expressapp.mjs
@@ -8,15 +8,46 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log("First middleware.");
-  throw new Error("A failure occurred!");
+  next();
 });
 
 app.use(router);
-router.get("/a", (req, res, next) => {
-  res.send("Hello this is route a");
+router.get("/api/v1/users", (req, res, next) => {
+  const users = [
+    {
+      id: 1,
+      username: "tom",
+    },
+    {
+      id: 2,
+      username: "jon",
+    },
+    {
+      id: 3,
+      username: "linda",
+    },
+  ];
+  console.log(req.query.userid);
+  const user = users.find((usr) => usr.id == req.query.userid);
+  res.send(`User ${user?.username}`);
 });
-router.post("/c", (req, res, next) => {
-  res.send(`Hello this is route c. Message is ${req.body.message}`);
+router.post("/api/v1/groups", (req, res, next) => {
+  const groups = [
+    {
+      id: 1,
+      groupname: "Admins",
+    },
+    {
+      id: 2,
+      groupname: "Users",
+    },
+    {
+      id: 3,
+      groupname: "Employees",
+    },
+  ];
+  const group = groups.find((grp) => grp.id == req.body.groupid);
+  res.send(`Group ${group.groupname}`);
 });
 
 app.use((err, req, res, next) => {
