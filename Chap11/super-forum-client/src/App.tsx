111a1e7b73e331e0c50c623307ae309ba58e495e
David Choi <dharric@live.com>
2020-08-11 16:07:43 -0400

    chap11 client start

---
+++ Chap11/super-forum-client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navigation">Nav</nav>
+      <div className="sidebar">Sidebar</div>
+      <div className="leftmenu">Left Menu</div>
+      <main className="content">Main</main>
+      <div className="rightmenu">Right Menu</div>
+    </div>
+  );
+}
+
+export default App;
