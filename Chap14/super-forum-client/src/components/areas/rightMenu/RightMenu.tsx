673c0ca6c5ae6335d91cb744184e108c2e549724
David Choi <davidchoi@Davids-MacBook-Pro.local>
2020-09-27 10:12:57 -0400

    chap14 RightMenu code

--- Chap14/super-forum-client/src/components/areas/rightMenu/RightMenu.tsx
+++ Chap14/super-forum-client/src/components/areas/rightMenu/RightMenu.tsx
@@ -1,27 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
-import { getTopCategories } from "../../../services/DataService";
 import TopCategory from "./TopCategory";
 import groupBy from "lodash/groupBy";
 import "./RightMenu.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GetTopCategoryThread = gql`
+  query getTopCategoryThread {
+    getTopCategoryThread {
+      threadId
+      categoryId
+      categoryName
+      title
+    }
+  }
+`;
 
 const RightMenu = () => {
+  const { data: categoryThreadData } = useQuery(GetTopCategoryThread);
   const { width } = useWindowDimensions();
   const [topCategories, setTopCategories] = useState<
     Array<JSX.Element> | undefined
   >();
 
   useEffect(() => {
-    getTopCategories().then((res) => {
-      const topCatThreads = groupBy(res, "category");
+    if (categoryThreadData && categoryThreadData.getTopCategoryThread) {
+      const topCatThreads = groupBy(
+        categoryThreadData.getTopCategoryThread,
+        "categoryName"
+      );
       const topElements = [];
       for (let key in topCatThreads) {
         const currentTop = topCatThreads[key];
         topElements.push(<TopCategory key={key} topCategories={currentTop} />);
       }
       setTopCategories(topElements);
-    });
-  }, []);
+    }
+  }, [categoryThreadData]);
 
   if (width <= 768) {
     return null;
--- Chap14/super-forum-client/src/models/CategoryThread.ts
+++ Chap14/super-forum-client/src/models/CategoryThread.ts
@@ -1,7 +1,7 @@
 export default class CategoryThread {
   constructor(
-      public threadId: string,
-      public category: string,
-      public title: string
-  ){}
-  }
+    public threadId: string,
+    public categoryName: string,
+    public title: string
+  ) {}
+}
