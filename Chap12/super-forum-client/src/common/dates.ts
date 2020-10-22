35e9a9ab4d854a67149a6598fd5ac9f7bc57b1a0
David Choi <admin@dzhaven.com>
2020-10-22 11:31:26 -0400

    replace moment with date-fns

--- Chap12/super-forum-client/src/common/dates.ts
+++ Chap12/super-forum-client/src/common/dates.ts
@@ -1,22 +1,19 @@
-import moment from "moment";
+import { format, differenceInMinutes } from "date-fns";
 
-const StandardDateTimeFormat = "MM/DD/YY";
+const StandardDateTimeFormat = "M/dd/yyyy";
 const getTimePastIfLessThanDay = (compTime: Date | null): string => {
   if (!compTime) return "";
 
-  const comparisonMoment = moment(compTime);
-  const now = moment();
-  const duration = moment.duration(now.diff(comparisonMoment));
-  const minutes = duration.asMinutes();
-
-  if (minutes > 60) {
-    const hours = duration.asHours();
-    if (hours > 24) {
-      return comparisonMoment.format(StandardDateTimeFormat);
+  const now = new Date();
+  const diffInMinutes = differenceInMinutes(now, compTime);
+  console.log("diff", diffInMinutes);
+  if (diffInMinutes > 60) {
+    if (diffInMinutes > 24 * 60) {
+      return format(compTime, StandardDateTimeFormat);
     }
-    return Math.round(hours) + "h ago";
+    return Math.round(diffInMinutes / 60) + "h ago";
   }
-  return Math.round(minutes) + "m ago";
+  return Math.round(diffInMinutes) + "m ago";
 };
 
 export { getTimePastIfLessThanDay };
