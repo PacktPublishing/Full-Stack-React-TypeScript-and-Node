fb891d66355331e78e22353dff3da2a36ba20e5e
stanikl <stanislav.lazarov@ibis-bg.com>
2022-01-15 19:24:20 +0100

    Test02

--- Chap1/test-age.ts
+++ Chap1/test-age.ts
@@ -1,21 +1,22 @@
 interface User {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
-function canDrive(usr: User) {     
-    console.log("user is", usr.name);     
-    
-    if(usr.age >= 16) {
-        console.log("allow to drive");
-    } else {
-        console.log("do not allow to drive");
-    }
-} 
-      
-const tom = { 
-    name: "tom",
-    age: 25
-} 
+function canDrive(usr: User) {
+  console.log("user is", usr.name);
+
+  if (usr.age >= 16) {
+    console.log("allow to drive");
+  } else {
+    console.log("do not allow to drive");
+  }
+}
+
+const tom = {
+  name: "tom",
+  age: 25,
+};
 
 canDrive(tom);
+//Das ist Test
